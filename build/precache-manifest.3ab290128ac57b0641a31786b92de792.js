5dfe3499576a6c4bd0889981034fbfb4b834fe1e
Chirag Bhalotia <me@chirag.codes>
2022-12-03 20:06:49 +0530

    updated

---
+++ build/precache-manifest.3ab290128ac57b0641a31786b92de792.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4250c6136c1cd4249a75cf9cfb7d1720",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e03b5d38f524a6f1b99",
+    "url": "/static/js/2.9534bcd7.chunk.js"
+  },
+  {
+    "revision": "f606733382a152a8c057",
+    "url": "/static/js/main.260959f1.chunk.js"
+  },
+  {
+    "revision": "330547137b78f9dce086",
+    "url": "/static/js/runtime-main.93af79aa.js"
+  }
+]);
